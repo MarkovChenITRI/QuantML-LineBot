022a123930e2ba8e7f75f508ac111e851ed5ed72
MarkovChenITRI <markov.chen1996@itri.org.tw>
2024-05-14 21:16:56 +0800

    update

---
+++ optimizer.py
@@ -0,0 +1,20 @@
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
+
+def tanh(x):
+  return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+def fit_regression(df, options, test_size = 0.05):
+    print(f'[optimizer.py] fit_regression()')
+    split_index = int(df.shape[0] * (1 - test_size))
+    X_col, y_col = [i for i in df if 'State' in i or 'Bias' in i], []
+    for market in options:
+    y_col += options[market]
+    input, label = np.array(df.loc[: , X_col]), np.array(df.loc[: , y_col])
+    X_train, y_train, X_test, y_test = input[: split_index], label[: split_index], input[split_index: ], label[split_index: ]
+
+    model = LinearRegression().fit(X_train, y_train)
+    score = model.score(X_test, y_test)
+    pred = model.predict(X_test[-1: ])
+    return pd.DataFrame(pred, columns=y_col).apply(tanh), score
