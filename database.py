1bba75eebcacfafbe89e74aeb459d9dc0dca9665
MarkovChenITRI <markov.chen1996@itri.org.tw>
2024-05-16 19:40:27 +0800

    Update database.py

--- database.py
+++ database.py
@@ -26,16 +26,17 @@ def Indexes(USD):
     res_df = res_df.replace(np.nan, None)
     for col in res_df.loc[:, ['price', 'eps', 'pe_ratio', 'beta', 'sharpo']]:
       res_df[col] = res_df[col].fillna(np.mean(res_df[col]))
-    """
+
     with driver.session() as session:
-      session.run("MATCH (u:utility WHERE u.name='{name}')  set u.update='{updated}' set u.price='{price}' set u.eps='{eps}'\
-                  set u.pe_ratio='{pe_ratio}'  set u.beta='{beta}'  set u.sharpo='{sharpo}'".format(name=u['name'], 
-                                                                                                    updated=updated,
-                                                                                                    price=str(price), 
-                                                                                                    eps=str(eps), 
-                                                                                                    pe_ratio=str(pe_ratio),
-                                                                                                    beta=str(beta),
-                                                                                                    sharpo=str(sharpo),
-                                                                                                    )
-                    )"""
+      for name, updated, price, eps, pe_ratio, beta, sharpo in res_df.values.tolist():
+        session.run("MATCH (u:utility WHERE u.name='{name}')  set u.update='{updated}' set u.price='{price}' set u.eps='{eps}'\
+                    set u.pe_ratio='{pe_ratio}'  set u.beta='{beta}'  set u.sharpo='{sharpo}'".format(name=name, 
+                                                                                                      updated=updated,
+                                                                                                      price=str(price), 
+                                                                                                      eps=str(eps), 
+                                                                                                      pe_ratio=str(pe_ratio),
+                                                                                                      beta=str(beta),
+                                                                                                      sharpo=str(sharpo),
+                                                                                                      )
+                    )
     return res_df
