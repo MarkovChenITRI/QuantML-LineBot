ebfb0479f591cbbdf6a0e1513dfe66ce9d7c365a
MarkovChenITRI <markov.chen1996@itri.org.tw>
2024-05-16 21:06:34 +0800

    Update optimizers.py

--- optimizers.py
+++ optimizers.py
@@ -26,7 +26,9 @@ def Fit_Regressor(df, options, test_size = 0.05):
     return df, score
 
 def Distribution_Optimizer(df, market_state, score):
+  print(f'[optimizer.py] Distribution_Optimizer()')
   risk_ratio = np.mean(market_state.loc[['Beta'], ['^DJI/Pred', '^GSPC/Pred', '^IXIC/Pred', '^TWII/Pred']]) * score
+  print(' - Risk Ratio:', risk_ratio)
   df["X"] = linprog(c    = list(df['sharpo'] * -1),
                     A_ub  =  [list(df['beta']),   [1 for _ in range(df.shape[0])]], #coefficient of variables for each constraint
                     b_ub  =  [risk_ratio, 1],    #y value of constraints
