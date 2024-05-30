1c0d265091bcaf4351c85fb3ecded478f04f3625
MarkovChenITRI <markov.chen1996@itri.org.tw>
2024-05-30 18:03:52 +0800

    update

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import functions_framework, base64, warnings, smtplib
-=======
-import base64
-#import functions_framework
->>>>>>> 0068617697d269fcb7321ef1bc0bdb0f9958a20a
 
 from config import Market
 from optimizers import Fit_Regressor, Shares_Optimizer
 from database import Indexes
-<<<<<<< HEAD
 from message import send_message
 from email.mime.text import MIMEText
 
@@ -16,15 +10,6 @@ import numpy as np
 
 @functions_framework.cloud_event
 def hello_pubsub(cloud_event):
-=======
-import warnings
-import smtplib
-from email.mime.text import MIMEText
-
-#@functions_framework.cloud_event
-#def hello_pubsub(cloud_event):
-def main():
->>>>>>> 0068617697d269fcb7321ef1bc0bdb0f9958a20a
     warnings.filterwarnings("ignore")
 
     #analysis
@@ -34,7 +19,6 @@ def main():
     analysis_df = Indexes(list(market_df['USDTWD=X'])[-1])
     analysis_df.loc[analysis_df.index[-1],:] = ['TWD', 'TWSE', '', 1, 1, 1, 1, 1]
 
-<<<<<<< HEAD
     #message
     timestamps = market_df.index
     period = int((timestamps[-1] - timestamps[0])/ np.timedelta64(1, 'Y'))
@@ -52,32 +36,18 @@ def main():
 
     df = Shares_Optimizer(analysis_df, market_state, options, score, ['UnitedStates', 'Taiwan'])
     df = df.iloc[:-4, ].loc[:, ["name", 'market', "pe_ratio", "beta", "sharpo", "X"]].round(2)
-=======
-    html = ''
-
-    df = Shares_Optimizer(analysis_df, market_state, options, score, ['UnitedStates', 'Taiwan'])
-    df = df.iloc[:-4, ].loc[:, ["name", 'market', "pe_ratio", "beta", "sharpo", "X"]].round(1)
->>>>>>> 0068617697d269fcb7321ef1bc0bdb0f9958a20a
     bull = df.where(df['X'] > 0).dropna().loc[:, ["name", 'market', "beta", "sharpo", "X"]]
     bull.columns = ["Name", 'Market', "Beta", "Sharpe Ratio", "Suggest Position"]
     html += "台/美股投資組合推薦<br>" + bull.to_html() + "<br>"
 
     df = Shares_Optimizer(analysis_df, market_state, options, score, ['UnitedStates'])
-<<<<<<< HEAD
     df = df.iloc[:-4, ].loc[:, ["name", 'market', "pe_ratio", "beta", "sharpo", "X"]].round(2)
-=======
-    df = df.iloc[:-4, ].loc[:, ["name", 'market', "pe_ratio", "beta", "sharpo", "X"]].round(1)
->>>>>>> 0068617697d269fcb7321ef1bc0bdb0f9958a20a
     bull = df.where(df['X'] > 0).dropna().loc[:, ["name", 'market', "beta", "sharpo", "X"]]
     bull.columns = ["Name", 'Market', "Beta", "Sharpe Ratio", "Suggest Position"]
     html += "美股投資組合推薦<br>" + bull.to_html() + "<br>"
 
     df = Shares_Optimizer(analysis_df, market_state, options, score, ['Taiwan'])
-<<<<<<< HEAD
     df = df.iloc[:-4, ].loc[:, ["name", 'market', "pe_ratio", "beta", "sharpo", "X"]].round(2)
-=======
-    df = df.iloc[:-4, ].loc[:, ["name", 'market', "pe_ratio", "beta", "sharpo", "X"]].round(1)
->>>>>>> 0068617697d269fcb7321ef1bc0bdb0f9958a20a
     bull = df.where(df['X'] > 0).dropna().loc[:, ["name", 'market', "beta", "sharpo", "X"]]
     bull.columns = ["Name", 'Market', "Beta", "Sharpe Ratio", "Suggest Position"]
     html += "台股投資組合推薦<br>" + bull.to_html() + "<br>"
@@ -85,11 +55,7 @@ def main():
     html += "本系統上的所有資訊僅供參考之用，並不構成財務或投資建議。在做出任何投資決定之前，我們建議您尋求獨立的財務建議。"
 
     #send email
-<<<<<<< HEAD
     whitelist = ["markov.chen1996@gmail.com", "Kepitlo@gmail.com"]
-=======
-    whitelist = ["markov.chen1996@gmail.com"]
->>>>>>> 0068617697d269fcb7321ef1bc0bdb0f9958a20a
 
     mime=MIMEText(html, "html", "utf-8")
     mime["Subject"]="本日投資組合及分配比例 (系統自動發送)"
@@ -105,9 +71,4 @@ def main():
     status=smtp.sendmail(from_addr, to_addr, msg)
     smtp.quit()
     print(status)
-<<<<<<< HEAD
     print(html)
-=======
-    print(html)
-main()
->>>>>>> 0068617697d269fcb7321ef1bc0bdb0f9958a20a
